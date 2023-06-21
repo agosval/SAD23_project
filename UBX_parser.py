320d99dea0beda17c89d935538a0e86f095990ee
Agostino <agos.vallefuoco@studenti.unina.it>
2023-06-21 20:34:50 +0200

    correzione ciclo for nel parser

--- UBX_parser.py
+++ UBX_parser.py
@@ -6,16 +6,19 @@ try:
     stream = open('COM10___115200_221206_130405.ubx', 'rb')
     ubr = UBXReader(stream, protfilter=2)
     print(type(ubr))
-    for i in range(1,50):
+    
+    for i in ubr:
         (raw_data,parsed_data)= ubr.read()
         #print(parsed_data)
-        temp= parsed_data.identity
+        id= parsed_data.identity
         #print(temp)
-        if temp == "NAV-HPPOSLLH" :
+        if id == "NAV-HPPOSLLH" :
             lat, lon, alt = parsed_data.lat, parsed_data.lon, parsed_data.hMSL
             print(f"lat = {lat}, lon = {lon}, alt = {alt/1000} m")
-        elif temp == "NAV-STATUS" :
-            print(f"STATUS m")
+        elif id == "HPPOSECEF":
+            print(f"HPPO")
+        elif id == "NAV-STATUS" :
+            print(f"STATUS ")
             
             
 except KeyboardInterrupt:
